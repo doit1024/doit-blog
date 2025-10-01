b6e7346d3590416b8c7fe9d1a69367462a058b25
展堂 <zhantang@qunhemail.com>
2025-10-01 17:18:40 +0800

    feat(rss): 全文rss推送 && 返回顶部按钮

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,35 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getCollection, render } from "astro:content";
 import { getPath } from "@/utils/getPath";
 import getSortedPosts from "@/utils/getSortedPosts";
 import { SITE } from "@/config";
+import { loadRenderers } from "astro:container";
+import { experimental_AstroContainer } from "astro/container";
+import { getContainerRenderer } from "@astrojs/mdx";
 
 export async function GET() {
+  const renderers = await loadRenderers([getContainerRenderer()]);
+  const container = await experimental_AstroContainer.create({ renderers });
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
+
+  const items = [];
+  for (const post of sortedPosts) {
+    const { data, id, filePath } = post;
+    const { Content } = await render(post);
+    const content = await container.renderToString(Content);
+    items.push({
+      ...post.data,
+      link: getPath(id, filePath),
+      content,
+      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+    });
+  }
+
   return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, id, filePath }) => ({
-      link: getPath(id, filePath),
-      title: data.title,
-      description: data.description,
-      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
-    })),
+    items,
   });
 }
